b3106a15e99a678704096acf839a12f34a0b68a4
Meghneel Gore <meghneelgore@gmail.com>
2018-03-10 13:19:28 -0800

    Removing deliberate failure in test.

--- src/test/java/com/meghneelgore/utils/maps/ImmutableHashMultiBimapTest.java
+++ src/test/java/com/meghneelgore/utils/maps/ImmutableHashMultiBimapTest.java
@@ -18,7 +18,6 @@ public class ImmutableHashMultiBimapTest {
         ImmutableList<String> list = ImmutableList.of("Hello", "World");
 
         Assert.assertEquals("Wrong inverse map", list, mapInverse.get(1));
-        Assert.assertFalse(true);
     }
 
     @Test
